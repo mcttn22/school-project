3a6d3fb7116da3b97ec574f38558fa20de61ce8e
Max Cotton <maxcotton22@gmail.com>
2023-07-26 19:38:18 +0100

    Setup base of xor_model.py structure

--- school_project/xor_model.py
+++ school_project/xor_model.py
@@ -1,39 +1,47 @@
 import math
+import numpy as np
 
 class XorModel():
     "ANN model that trains to predict the output of a XOR gate with two inputs"
     def __init__(self) -> None:
         "Initialise model values"
-        self.biases: list[float, float] = [0,0]
-        self.weights: list[float, float] = [0,0]
-        self.inputs: list[int] = [0,1]
+        self.hidden_bias: float = 0
+        self.output_bias: float = 0
+        self.hidden_weight: float = 0
+        self.output_weight: float = 0
+        self.LEARNING_RATE: float = 0.1
+        self.inputs = np.array([[0,0,1,1],
+                               [0,1,0,1]])
+        self.outputs = np.array([0,1,1,0])
 
     def __repr__(self) -> str:
         "Read current state of model"
-        return f"Biases: {', '.join(str(i) for i in self.biases)}\nWeights: {', '.join(str(i) for i in self.weights)}"
+        return f"Biases: {', '.join(str(i) for i in self.biases)}\nWeights: {', '.join(str(i) for i in self.weights)}\nLearning Rate: {self.LEARNING_RATE}"
 
-    def predict(self):
+    def sigmoid(self, z: float) -> float:
+        "Transfer function, transforms input to number between 0 and 1"
+        return 1 / (1 + math.expr(-z))
+
+    def cost_gradient(self, cost: float) -> float:
+        "Calculate the gradient of the cost function"
+
+    def cost(self) -> float:
+        "Calculate the error in the prediction"
+
+    def back_propagation(self) -> None:
+        "Adjust the weights and biases based on the cost of the prediction"
+    
+    def forward_propagation(self):
+        "Generate a prediction with the weights and biases"
+
+    def predict(self) -> None:
         "Use trained weights and biases to predict ouput of XOR gate on two inputs"
         print("\n*** Using trained weights and biases to predict output of XOR gate on two inputs ***\n")
         print("\nFinal state of model:\n")
         print(self)
 
-    def sigmoid(self, z: int) -> int:
-        "Transfer function, transforms input to number between 0 and 1"
-        return 1 / (1 + math.expr(-z))
-
-    def train(self):
+    def train(self, epochs: int) -> None:
         "Train weights and biases"
         print("\n*** Training Weights and Biases ***\n")
-        # for epoch in range(100):
-        #     pass
-
-    def model(self):
-        "Manages model"
-        self.train()
-        self.predict()
-
-    def main(self):
-        "Entrypoint of XOR model"
-        print("XOR model")
-        self.model()
+        for epoch in range(epochs):
+            pass
