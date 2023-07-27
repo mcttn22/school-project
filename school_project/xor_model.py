030833780f4aaa42d707a0d85d501b5de356f75c
Max Cotton <maxcotton22@gmail.com>
2023-07-27 20:59:34 +0100

    First xor model attempt

--- school_project/xor_model.py
+++ school_project/xor_model.py
@@ -1,38 +1,45 @@
-import math
+import matplotlib.pyplot as plt
 import numpy as np
 
 class XorModel():
     "ANN model that trains to predict the output of a XOR gate with two inputs"
     def __init__(self) -> None:
         "Initialise model values"
-        self.hidden_bias: float = 0
-        self.output_bias: float = 0
-        self.hidden_weight: float = 0
-        self.output_weight: float = 0
+        # Setup pseudo random values for weight arrays
+        np.random.seed(2)
+        self.hiddenWeights = np.random.rand(2, 2)
+        self.outputWeights = np.random.rand(1, 2)
         self.LEARNING_RATE: float = 0.1
         self.inputs = np.array([[0,0,1,1],
                                [0,1,0,1]])
-        self.outputs = np.array([0,1,1,0])
+        self.outputs = np.array([[0,1,1,0]])
 
     def __repr__(self) -> str:
         "Read current state of model"
-        return f"Biases: {', '.join(str(i) for i in self.biases)}\nWeights: {', '.join(str(i) for i in self.weights)}\nLearning Rate: {self.LEARNING_RATE}"
+        return f"Hidden Weights: {self.hiddenWeights}\nOutput Weights: {self.outputWeights}\nLearning Rate: {self.LEARNING_RATE}"
 
-    def sigmoid(self, z: float) -> float:
+    def sigmoid(self, z):
         "Transfer function, transforms input to number between 0 and 1"
-        return 1 / (1 + math.expr(-z))
+        return 1 / (1 + np.expr(-z))
 
-    def cost_gradient(self, cost: float) -> float:
-        "Calculate the gradient of the cost function"
+    def back_propagation(self, hiddenOutput, prediction) -> None:
+        "Adjust the weights and biases via gradient descent"
+        outputWeightGradient = np.dot(prediction - self.outputs, hiddenOutput.T) / self.inputs.shape[1]   # Why divide by m ?
+        hiddenWeightGradient = np.dot(np.dot(prediction - self.outputs, self.outputWeights.T) * hiddenOutput * (1 - hiddenOutput), self.inputs.T) / self.inputs.shape[1]
+        # Reshape arrays
+        outputWeightGradient = np.reshape(outputWeightGradient, self.outputWeights.shape)
+        hiddenWeightGradient = np.reshape(hiddenWeightGradient, self.hiddenWeights.shape)
+        # Update weights
+        self.hiddenWeights -= self.LEARNING_RATE * hiddenWeightGradient
+        self.outputWeights -= self.LEARNING_RATE * outputWeightGradient
 
-    def cost(self) -> float:
-        "Calculate the error in the prediction"
-
-    def back_propagation(self) -> None:
-        "Adjust the weights and biases based on the cost of the prediction"
-    
     def forward_propagation(self):
-        "Generate a prediction with the weights and biases"
+        "Generate a prediction with the weights and biases, returns the hidden layer output and a prediction"
+        z1 = np.dot(self.hiddenWeights, self.inputs)
+        hiddenOutput = self.sigmoid(z1)
+        z2 = np.dot(self.outputWeights, hiddenOutput)
+        prediction = self.sigmoid(z2)
+        return hiddenOutput, prediction
 
     def predict(self) -> None:
         "Use trained weights and biases to predict ouput of XOR gate on two inputs"
@@ -43,5 +50,12 @@ class XorModel():
     def train(self, epochs: int) -> None:
         "Train weights and biases"
         print("\n*** Training Weights and Biases ***\n")
+        losses: list[float] = []
         for epoch in range(epochs):
-            pass
+            hiddenOutput, prediction = self.forward_propagation()
+            loss = - (1/self.inputs.shape[1]) * np.sum(self.outputs * np.log(prediction) + (1 - self.outputs) * np.log(1 - prediction))
+            losses.append(loss)
+            self.back_propagation(hiddenOutput=hiddenOutput, prediction=prediction)
+        plt.plot(losses)
+        plt.xlabel("Epochs")
+        plt.ylabel("Loss Value")
