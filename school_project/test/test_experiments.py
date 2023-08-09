73fe3103c170ce96178f2dbb011779800d024195
Max Cotton <maxcotton22@gmail.com>
2023-08-09 21:55:11 +0100

    Add doc to tests

--- school_project/test/test_experiments.py
+++ school_project/test/test_experiments.py
@@ -7,10 +7,15 @@ class TestXorModel(unittest.TestCase):
         self.xor_model = XorModel()
 
     def test_sigmoid(self) -> None:
-        "Test sigmoid output range to be within 0-1"
-        inputs = [-100,0,100]
-        for i in inputs:
-            output = self.xor_model.sigmoid(i)
+        """Test sigmoid output range to be within 0-1.
+        
+        Raises:
+            AssertionError: if sigmoid output range is not within 0-1.
+        
+        """
+        test_inputs = [-100,0,100]
+        for test_input in test_inputs:
+            output = self.xor_model.sigmoid(test_input)
             self.assertTrue(output >= 0 and output <= 1, "Sigmoid should return a number between 0 and 1")
 
 if __name__ == '__main__':
