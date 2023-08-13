883f6d4ebef1ced80ac887b9aa2b76bfc186f779
Max Cotton <maxcotton22@gmail.com>
2023-08-13 21:28:57 +0100

    Add biases to xor model

--- school_project/experiments.py
+++ school_project/experiments.py
@@ -23,20 +23,25 @@ class XorModel():
         
         # Setup model attributes
         self.running: bool = True
-        self.input_count: int = self.train_inputs.shape[0]
+        self.input_neuron_count: int = self.train_inputs.shape[0]
         self.hidden_neuron_count: int = 4
         self.output_neuron_count: int = 1
         
-        # Initialise weights to random values
+        # Initialise weights to random values and biases to 0s
         np.random.seed(2)  # Sets up pseudo random values for weight arrays
         self.hidden_weights: np.ndarray = np.random.rand(
                                                      self.hidden_neuron_count,
-                                                     self.input_count
+                                                     self.input_neuron_count
                                                      )
         self.output_weights: np.ndarray = np.random.rand(
                                                      self.output_neuron_count,
                                                      self.hidden_neuron_count
                                                      )
+        self.hidden_biases: np.ndarray = np.zeros(
+                                          shape=(self.hidden_neuron_count, 1)
+                                          )
+        self.output_biases: np.ndarray = np.zeros(
+                                          shape=(self.output_neuron_count, 1))
         self.LEARNING_RATE: float = 0.1
 
     def __repr__(self) -> str:
@@ -50,14 +55,21 @@ class XorModel():
         return (f"Number of hidden neurons: {self.hidden_neuron_count}\n" +
                 f"Hidden Weights: {self.hidden_weights.tolist()}\n" +
                 f"Output Weights: {self.output_weights.tolist()}\n" +
+                f"Hidden biases: {self.hidden_biases.tolist()}\n" +
+                f"Output biases: {self.output_biases.tolist()}\n" +
                 f"Learning Rate: {self.LEARNING_RATE}")
 
     def init_weights(self) -> None:
-        """Initialise weights to randdom values."""
+        """Initialise weights to randdom values and biases to 0s"""
         self.hidden_weights = np.random.rand(self.hidden_neuron_count,
-                                             self.input_count)
+                                             self.input_neuron_count)
         self.output_weights = np.random.rand(self.output_neuron_count,
                                              self.hidden_neuron_count)
+        self.hidden_biases: np.ndarray = np.zeros(
+                                          shape=(self.hidden_neuron_count, 1)
+                                          )
+        self.output_biases: np.ndarray = np.zeros(
+                                          shape=(self.output_neuron_count, 1))
 
     def sigmoid(self, z: np.ndarray | int | float) -> np.ndarray | float:
         """Transfer function, transform input to number between 0 and 1.
@@ -79,37 +91,43 @@ class XorModel():
         """Adjust the weights via gradient descent.
         
         Args:
+            hidden_output (numpy.ndarray): the matrice of hidden output values
             prediction (numpy.ndarray): the matrice of prediction values
         Raises:
             ValueError:
-            if prediction is not a suitable multiplier with the weights
+            if prediction or hidden_output
+            is not a suitable multiplier with the weights
             (incorrect shape)
         
         """
         output_weight_gradient: np.ndarray = np.dot(prediction - self.train_outputs, hidden_output.T) / self.train_inputs.shape[1]
         hidden_weight_gradient: np.ndarray = np.dot(np.dot(self.output_weights.T, prediction - self.train_outputs) * hidden_output * (1 - hidden_output), self.train_inputs.T) / self.train_inputs.shape[1]
-        
+        output_bias_gradient: np.ndarray = np.sum(prediction - self.train_outputs) / self.train_inputs.shape[1]
+        hidden_bias_gradient: np.ndarray = np.sum(np.dot(self.output_weights.T, prediction - self.train_outputs) * hidden_output * (1 - hidden_output)) / self.train_inputs.shape[1]
+
         # Reshape arrays to match the weight arrays for multiplication
         output_weight_gradient = np.reshape(output_weight_gradient,
                                             self.output_weights.shape)
         hidden_weight_gradient = np.reshape(hidden_weight_gradient,
                                             self.hidden_weights.shape)
         
-        # Update weights
-        self.hidden_weights -= self.LEARNING_RATE * hidden_weight_gradient
+        # Update weights and biases
         self.output_weights -= self.LEARNING_RATE * output_weight_gradient
+        self.hidden_weights -= self.LEARNING_RATE * hidden_weight_gradient
+        self.output_biases -= self.LEARNING_RATE * output_bias_gradient
+        self.hidden_biases -= self.LEARNING_RATE * hidden_bias_gradient
 
     def forward_propagation(self) -> tuple[np.ndarray, np.ndarray]:
-        """Generate a prediction with the weights and bias.
+        """Generate a prediction with the weights and biases.
         
         Returns:
             a numpy.ndarray of the hidden output values
             and a numpy.ndarray of prediction values.
 
         """
-        z1: np.ndarray = np.dot(self.hidden_weights, self.train_inputs)
+        z1: np.ndarray = np.dot(self.hidden_weights, self.train_inputs) + self.hidden_biases
         hidden_output: np.ndarray = self.sigmoid(z1)
-        z2: np.ndarray = np.dot(self.output_weights, hidden_output)
+        z2: np.ndarray = np.dot(self.output_weights, hidden_output) + self.output_biases
         prediction: np.ndarray = self.sigmoid(z2)
         return hidden_output, prediction
 
@@ -124,7 +142,7 @@ class XorModel():
                                               ) * 100
 
     def train(self, epochs: int) -> None:
-        """Train weights.
+        """Train weights and biases.
         
         Args:
             epochs (int): the number of forward and back propagations to do.
