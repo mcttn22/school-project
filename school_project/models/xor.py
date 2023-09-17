2f9affbf39513e2321c569c1d74e0d12d9aa115b
Max Cotton <maxcotton22@gmail.com>
2023-09-18 00:05:52 +0100

    Add CuPy to XOR model

--- school_project/models/xor.py
+++ school_project/models/xor.py
@@ -1,4 +1,4 @@
-import numpy as np
+import cupy as cp
 
 from school_project.models.utils.deep_model import AbstractDeepModel
 
@@ -7,20 +7,20 @@ class DeepModel(AbstractDeepModel):
       that trains to predict the output of a XOR gate with two inputs."""
     def __init__(self) -> None:
         """Initialise Model's Base class."""
-        super().__init__(hidden_layers_shape=[2, 2],
+        super().__init__(hidden_layers_shape=[2],
                          learning_rate=1)
 
-    def load_datasets(self) -> tuple[np.ndarray, np.ndarray, 
-                                     np.ndarray, np.ndarray]:
+    def load_datasets(self) -> tuple[cp.ndarray, cp.ndarray, 
+                                     cp.ndarray, cp.ndarray]:
         """Load XOR input and output datasets.
         
         Returns:
             tuple of XOR train_inputs, train_outputs,
-            test_inputs and test_outputs numpy.ndarrys.
+            test_inputs and test_outputs cupy.ndarrys.
 
         """
-        inputs: np.ndarray = np.array([[0, 0, 1, 1],
+        inputs: cp.ndarray = cp.array([[0, 0, 1, 1],
                                        [0, 1, 0, 1]])
-        outputs: np.ndarray = np.array([[0, 1, 1, 0]])
+        outputs: cp.ndarray = cp.array([[0, 1, 1, 0]])
 
         return inputs, outputs, inputs, outputs
