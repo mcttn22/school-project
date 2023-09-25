6551331ed7d692b33d7fb5aabb57a1e125027506
Max Cotton <maxcotton22@gmail.com>
2023-09-25 08:23:58 +0100

    Update Epoch Count

--- school_project/pages/cat_recognition.py
+++ school_project/pages/cat_recognition.py
@@ -241,7 +241,7 @@ class CatRecognitionFrame(tk.Frame):
                                           fg='red')
         train_thread: threading.Thread = threading.Thread(
                                            target=self.deep_model.train,
-                                           args=(2_000,)
+                                           args=(3_500,)
                                            )
         train_thread.start()
         self.manage_training(train_thread=train_thread)
