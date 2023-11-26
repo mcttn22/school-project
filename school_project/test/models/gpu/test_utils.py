408081fbf99ffaf631a86fdc9e331097bb3147b6
Max Cotton <maxcotton22@gmail.com>
2023-11-26 22:55:01 +0000

    Add ReLu unit test

--- school_project/test/models/gpu/test_utils.py
+++ school_project/test/models/gpu/test_utils.py
@@ -17,6 +17,21 @@ class TestTools(unittest.TestCase):
             output = tools.sigmoid(test_input)
             self.assertTrue(output >= 0 and output <= 1,
                             "Sigmoid should return a number between 0 and 1")
+            
+    def test_relu(self) -> None:
+        """Test ReLu output range to be >=0.
+        
+        Raises:
+            AssertionError: if relu output range is not >=0.
+        
+        """
+        test_inputs = [-100,0,100]
+        for test_input in test_inputs:
+            output = tools.relu(test_input)
+            self.assertTrue(
+                      output >= 0,
+                      "ReLu should return a number greater than or equal to 0"
+                      )
 
 if __name__ == '__main__':
     unittest.main()
