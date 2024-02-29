f337f1f00510d9ab8c015bf9a3d995802ddb1b46
Max Cotton <maxcotton22@gmail.com>
2024-02-29 18:40:47 +0000

    Tidy database unit tests

--- school_project/test/test_database.py
+++ school_project/test/test_database.py
@@ -7,15 +7,15 @@ import uuid
 class TestDatabase(unittest.TestCase):
     """Unit tests for database."""
     def __init__(self, *args, **kwargs) -> None:
-        """Initialise units tests and inputs"""
+        """Initialise unit tests."""
         super(TestDatabase, self).__init__(*args, **kwargs)
 
     def test_database_strucure(self) -> None:
-        """Test that the tables and table attributes are set up correctly."""
+        """Test that the database tables are set up correctly."""
         connection = sqlite3.connect(database='school_project/saved_models.db')
         cursor = connection.cursor()
 
-        # Check that 'Models' table is in database
+        # Check that 'Models' table is in the database
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
         self.assertIn(member="Models", container=cursor.fetchall()[0])
 
@@ -30,16 +30,17 @@ class TestDatabase(unittest.TestCase):
                                (7, 'Use_ReLu', 'INTEGER', 1, None, 0)]
         cursor.execute("PRAGMA table_info(Models)")
         table_info = cursor.fetchall()
-        for expected_attribute, attribute in zip (expected_table_info, table_info):
+        for expected_attribute, attribute in zip (expected_table_info,
+                                                  table_info):
             for expected_info, info in zip(expected_attribute, attribute):
                 self.assertEqual(first=expected_info, second=info)
 
     def test_not_null_constraint(self) -> None:
-        """Test that a the NOT NULL constraint is setup."""
+        """Test that the NOT NULL constraint is setup."""
         connection = sqlite3.connect(database='school_project/saved_models.db')
         cursor = connection.cursor()
 
-        # Try to insert with the last attribute missing
+        # Try to insert record with the last attribute missing
         test_data = ("Test_Dataset", 
                      f"school_project/saved-models/{uuid.uuid4().hex}.npz",
                      "100, 100",
@@ -97,8 +98,8 @@ class TestDatabase(unittest.TestCase):
         cursor.execute(sql, parameters)
         connection.commit()
 
-    def test_save_retrieve_consistency(self) -> None:
-        """Test that data is not changed between saving and retrieving."""
+    def test_save_load_consistency(self) -> None:
+        """Test that data is not changed between saving and loading."""
         connection = sqlite3.connect(database='school_project/saved_models.db')
         cursor = connection.cursor()
         
@@ -119,14 +120,14 @@ class TestDatabase(unittest.TestCase):
         cursor.execute(sql, test_data)
         connection.commit()
 
-        # Retrieve test data
+        # Load test data
         sql = """
         SELECT * FROM Models WHERE Dataset=? AND Name=?
         """
         cursor.execute(sql, (test_data[0], test_data[4]))
-        retrieved_data = cursor.fetchall()[0]
+        loaded_data = cursor.fetchall()[0]
 
-        # Remove test data from database
+        # Delete test data from database
         sql = """
         DELETE FROM Models WHERE Dataset=? AND Name=?
         """
@@ -134,6 +135,6 @@ class TestDatabase(unittest.TestCase):
         cursor.execute(sql, parameters)
         connection.commit()
 
-        # Compare initial test data with retrieved data
-        for test_value, retrieved_value in zip(test_data, retrieved_data[1:]):
-            self.assertEqual(first=test_value, second=retrieved_value)
+        # Compare test data with loaded data
+        for test_value, loaded_value in zip(test_data, loaded_data[1:]):
+            self.assertEqual(first=test_value, second=loaded_value)
